1d0e9a60ed8641f4e69d6b00f4565ad538abf1df
sanctim <ksgisong78@gmail.com>
2018-11-29 12:29:50 +0900

    jhuhgh

--- Spring06_JSONResponse/src/main/java/com/gura/spring06/FileController.java
+++ Spring06_JSONResponse/src/main/java/com/gura/spring06/FileController.java
@@ -1,23 +1,24 @@
 package com.gura.spring06;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
 
+@Controller
 public class FileController {
-	@RequestBody
+	
+	@RequestMapping("/download")
 	public ModelAndView download(@RequestParam int num) {
 		//ModelAndView 객체를 생성해서 
 		ModelAndView mView=new ModelAndView();
-		//다운로드 시켜줄 파일의 정보를 담고
-		mView.addObject("num",num);
-		mView.addObject("saveFileName","kimgura.jpg");
+		//다운로드 시켜줄 파일의 정보를 담고 
+		mView.addObject("num", num);
+		mView.addObject("saveFileName", "kimgura.jpg");
 		//다운로드 시켜줄 view 페이지의 정보를 담아서 
-		mView.setViewName("fileDownView");   //Web-inf/view/fileDownView.jsp
-		//리턴해 준다.
-		return  mView;
+		mView.setViewName("fileDownView");
+		//리턴해준다. 
+		return mView;
 	}
 }
